82f003b7cb65cc469a5d8d740096d33743cdad3e
Ayesh <ayeshqumhieh@gmail.com>
2024-09-26 20:56:40 +0300

    Notes Repository Implementation

--- composeApp/src/commonMain/kotlin/com/kmm/notes/repo/NotesRepository.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/notes/repo/NotesRepository.kt
@@ -1,3 +1,16 @@
 package com.kmm.notes.repo
 
-interface NotesRepository
+import com.kmm.notes.entity.Note
+
+interface NotesRepository {
+
+    suspend fun getAllNotes(): List<Note>
+
+    suspend fun addNote(note: Note)
+
+    suspend fun getNoteById(id: Long): Note?
+
+    suspend fun updateNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+}
--- composeApp/src/commonMain/kotlin/com/kmm/notes/repo/NotesRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/notes/repo/NotesRepositoryImpl.kt
@@ -1,12 +1,56 @@
 package com.kmm.notes.repo
 
 import com.kmm.notes.db.DatabaseDriverFactory
+import com.kmm.notes.db.NotesDatabase
+import com.kmm.notes.entity.Note
 
 class NotesRepositoryImpl(
-    private val databaseDriverFactory: DatabaseDriverFactory
+    databaseDriverFactory: DatabaseDriverFactory
 ) : NotesRepository {
 
-    init {
-        println("databaseDriverFactory ref: ${databaseDriverFactory.hashCode()}")
+    private val database = NotesDatabase(databaseDriverFactory.createDriver())
+
+    private val dbQuery = database.notesDatabaseQueries
+
+    override suspend fun getAllNotes(): List<Note> =
+        dbQuery.selectAllNotes(::mapNote).executeAsList()
+
+    override suspend fun addNote(note: Note) {
+        dbQuery.insertNote(
+            title = note.title,
+            content = note.content
+        )
+    }
+
+    override suspend fun getNoteById(id: Long): Note? =
+        dbQuery
+            .selectNoteById(
+                id,
+                ::mapNote
+            ).executeAsOneOrNull()
+
+    override suspend fun updateNote(note: Note) {
+        dbQuery
+            .updateNotByid(
+                id = note.id,
+                title = note.title,
+                content = note.content
+            )
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dbQuery.deleteNoteById(note.id)
+    }
+
+    private fun mapNote(
+        id: Long,
+        title: String,
+        content: String
+    ): Note {
+        return Note(
+            id = id,
+            title = title,
+            content = content
+        )
     }
 }
