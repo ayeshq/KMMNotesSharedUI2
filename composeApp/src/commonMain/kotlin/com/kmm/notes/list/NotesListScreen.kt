fe192438a4d1ebf22b5c6281715ebc5bdaa2111b
Ayesh <ayeshqumhieh@gmail.com>
2024-09-26 21:47:58 +0300

    Basic NotesList Layout, and Basic Fetch notes in the view model

--- composeApp/src/commonMain/kotlin/com/kmm/notes/list/NotesListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/notes/list/NotesListScreen.kt
@@ -1,14 +1,29 @@
 package com.kmm.notes.list
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.kmm.notes.entity.Note
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -17,24 +32,68 @@ fun NotesListScreen(
     editNote: (Long) -> Unit
 ) {
     val viewModel = koinViewModel<NotesListViewModel>()
+    val state by remember { viewModel.state }
 
     Scaffold(
         modifier = modifier,
         topBar = {
             TopAppBar(
-                title = { Text("Notes") }
+                title = {
+                    Text(
+                        "KMM Notes",
+                        style = MaterialTheme.typography.h5
+                    )
+                }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                //Navigate to the edit screen, with -1L as the note ID to indicate a new note
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+            }
         }
     ) {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             contentAlignment = Alignment.Center
         ) {
-            Button(onClick = {
-                editNote(0L)
-            }) {
-                Text(text = "Edit Note")
+            if (state.isEmpty) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = "You don't have any notes yet! You can add a new note by clicking on the + button",
+                        style = MaterialTheme.typography.subtitle2,
+                        modifier = modifier.padding(16.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            } else {
+                LazyColumn(
+                    modifier = modifier.padding(vertical = 4.dp)
+                ) {
+                    val onNoteClicked: (Long) -> Unit = { noteId ->
+                        editNote(noteId)
+                    }
+
+                    items(items = state.notes) {
+                        NoteCard(it, onNoteClicked)
+                    }
+                }
             }
         }
     }
+}
+
+@Composable
+fun NoteCard(
+    note: Note,
+    onClick: (Long) -> Unit
+) {
+    //Column {Title, Partial content}
 }
--- composeApp/src/commonMain/kotlin/com/kmm/notes/list/NotesListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/notes/list/NotesListViewModel.kt
@@ -1,13 +1,43 @@
 package com.kmm.notes.list
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kmm.notes.entity.Note
 import com.kmm.notes.repo.NotesRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class NotesListViewModel(
     private val notesRepository: NotesRepository
 ) : ViewModel() {
 
+    private val errorHandler = CoroutineExceptionHandler { _, _ ->
+        _state.value = _state.value.copy(isError = true)
+    }
+
+    private val _state = mutableStateOf(NotesListScreenState())
+    val state: State<NotesListScreenState> = _state
+
     init {
-        println("Notes Repository Ref: ${notesRepository.hashCode()}")
+        loadAllNotes()
+    }
+
+    private fun loadAllNotes() {
+        viewModelScope.launch(errorHandler) {
+            val notes = notesRepository.getAllNotes()
+            _state.value = _state.value.copy(
+                isEmpty = notes.isEmpty(),
+                isError = false,
+                notes = notes
+            )
+        }
     }
 }
+
+data class NotesListScreenState(
+    val isEmpty: Boolean = false,
+    val isError: Boolean = false,
+    val notes: List<Note> = emptyList()
+)
